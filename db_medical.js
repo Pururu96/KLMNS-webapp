51ea8625b8948bd6a8d9d1a57f9c95d05f8dff1a
simelane-jpd <201575227@student.uj.ac.za>
2021-11-17 11:25:30 +0200

    removed mysql

--- db_medical.js
+++ db_medical.js
@@ -1,87 +1,86 @@
-var mysql = require("mysql");
+//var sqlite3 = require("sqlite3");
 
+//var express = require("express");
+//var router = express.Router();
 
-var express = require("express");
-var router = express.Router();
+//var con = sqlite3.createConnection({
+  //host: "localhost",
+  //port: "3017",
+  //user: "root",
+  //password: "mypassword",
+  //database: "nodesqlite3",
+//});
 
-var con = mysql.createConnection({
-  host: "localhost",
-  port: "3017",
-  user: "root",
-  password: "mypassword",
-  database: "nodemysql",
-});
-
-con.connect(function (err) {
-  if (err) {
-    throw err;
-  } else {
-    console.log("you are connected");
+//con.connect(function (err) {
+  //if (err) {
+    //throw err;
+  //} else {
+    //console.log("you are connected");
     //console.log({
       //host            : process.env.MYSQL_HOST,
       //user            : process.env.MYSQL_USER,
       //password        : process.env.MYSQL_SECRET,
      // database        : process.env.MYSQL_DB
    //})
-    }
-    });
+    //}
+    //});
 
 
   
 
-module.exports.personal = function (
-    patient_name,
-    patient_surname,
-    date_of_birth,
-    gender,
-    race,
-    address_1,
-    address_2,
-    city,
-    province,
-    postal_code,
-    cell_no,
-    tell_no,
-    email,
-    medical,
-    agree
+//module.exports.personal = function (
+    //patient_name,
+    //patient_surname,
+    //date_of_birth,
+    //gender,
+    //race,
+    //address_1,
+    //address_2,
+    //city,
+    //province,
+    //postal_code,
+    //cell_no,
+    //tell_no,
+    //email,
+    //medical,
+    //agree
     
     
     
     
-  ) {
-    var query =
-      "INSERT INTO `personal`(`patient_name`,`patient_surname`, `date_of_birth`, `gender`, `race`, `address_1`, `address_2`, `city`, `province`,`postal_code`,`cell_no`,`tell_no`, `email`,`medical`, `agree`) values ('" +
-      patient_name +
-      "','" +
-      patient_surname +
-      "','" +
-      date_of_birth +
-      "','" +
-      gender +
-      "','" +
-      race +
-      "','" +
-      address_1 +
-      "','" +
-      address_2 +
-      "','" +
-      city +
-      "','" +
-      province +
-      "','" +
-      postal_code +
-      "','" +
-      cell_no +
-      "','" +
-      tell_no +
-      "','" +
-      email +
-      "','" +
-      medical +
-      "','" +
-      agree +
-      "')";
-    con.query(query, callback);
-    console.log(query);
-  };
+  //) {
+    //var query =
+      //"INSERT INTO `personal`(`patient_name`,`patient_surname`, `date_of_birth`, `gender`, `race`, `address_1`, `address_2`, `city`, `province`,`postal_code`,`cell_no`,`tell_no`, `email`,`medical`, `agree`) values ('" +
+      //patient_name +
+      //"','" +
+      //patient_surname +
+      //"','" +
+      //date_of_birth +
+      //"','" +
+      //gender +
+      //"','" +
+      //race +
+      //"','" +
+      //address_1 +
+     // "','" +
+      //address_2 +
+      //"','" +
+      //city +
+      //"','" +
+      //province +
+      //"','" +
+      //postal_code +
+      //"','" +
+      //cell_no +
+      //"','" +
+      //tell_no +
+      //"','" +
+      //email +
+      //"','" +
+      //medical +
+      //"','" +
+      //agree +
+      //"')";
+    //con.query(query, callback);
+    //console.log(query);
+  //};
